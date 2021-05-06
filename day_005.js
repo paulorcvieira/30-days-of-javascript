451bbdc12982c65812d855e0909d49bafc05391b
Paulo Vieira <paullocostta86@gmail.com>
2021-05-06 02:21:11 -0300

    challenge: day 05 add sort with localeCompare

--- day_005.js
+++ day_005.js
@@ -3,11 +3,17 @@ const people = [
   { name: "Paulo", age: "25" },
   { name: "Caroline", age: "26" },
   { name: "Carmen", age: "27" },
+  { name: "Débora", age: "29" },
   { name: "Debora", age: "28" },
 ];
 people.sort(function(a, b) {
   return a.name < b.name ? -1 : 1;
 });
+console.log(people);
+people.sort( (a, b) => a.name.localeCompare(b.name, 'pt-BR', {
+  sensitivity: 'variant',
+  ignorePunctuation: true
+}));
 console.log(people);
 
 // [{ name: 'Carmen', age: '27' },
